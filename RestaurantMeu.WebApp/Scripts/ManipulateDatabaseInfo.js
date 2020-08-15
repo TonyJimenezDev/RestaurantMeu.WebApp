6020ca1acdedb322b27f15758b7c3e3295d9a190
TonyJimenezDev <TonyJimenezDev@outlook.com>
2020-08-15 16:42:39 -0500

    Tax + total tax + total price added Next remove needs to update final price

--- RestaurantMeu.WebApp/Scripts/ManipulateDatabaseInfo.js
+++ RestaurantMeu.WebApp/Scripts/ManipulateDatabaseInfo.js
@@ -1,22 +1,22 @@
 ﻿/* Jquery dependent*/
 
 $(document).ready(function () {
-    $("#Item").val(0);
-    $("#Item").change(function () { /* Drop down item name */
-        var itemId = $("#Item").val();
+    $("#item").val(0);
+    $("#item").change(function () { /* Drop down item name */
+        var itemId = $("#item").val();
         $("#txtQuantity").val(0);
         $("#txtTotal").val("0.00");
         GetItemPrice(itemId);
     });
     $("input[type=text]").change(function () { /*Fires off input type=text */
-        CalculateTax();
+        CalculateSalesTax();
         CalculateSubTotal();
     })
     $("#txtQuantity").change(function () { /*Fires off on Quantity */
-        CalculateTax();
+        CalculateSalesTax();
         CalculateSubTotal();
     })
-    $("#btnAddToList").click(function () {
+    $("#btnAddToList").click(function () { /*Fires off on button add */
         AddToTheItemList();
     })
 });
@@ -26,23 +26,37 @@ function AddToTheItemList() {
     var unitPrice = $("#txtPrice").val();
     var quantity = $("#txtQuantity").val();
     var discount = $("#txtDiscount").val();
-    var itemId = $("#Item").val(); /* Hidden field */
-    var itemName = $("#Item option:selected").text();
+    var itemId = $("#item").val(); /* Hidden field */
+    var itemName = $("#item option:selected").text();
+    var tax = $("#txtTax").val();
     var Total = (unitPrice * quantity) - discount;
 
-    var itemList = "<tr><td hidden>" +
-        itemId + "</td><td>" +
-        itemName + "</td><td>" +
-        unitPrice + "</td><td>" +
-        quantity + "</td><td>" +
-        parseFloat(discount).toFixed(2) + "</td><td>" +
-        parseFloat(Total).toFixed(2) + "</td><td> <input type='button' value='Remove' name='remove' class='btn btn-danger' onclick='RemoveItem(this)' /> </td></tr>";
+    var itemList = "<tr><td hidden>" + itemId + "</td>" +
+        "<td>" + itemName + "</td>" +
+        "<td>" + unitPrice + "</td>" +
+        "<td>" + quantity + "</td>" +
+        "<td>" + parseFloat(discount).toFixed(2) + "</td>" +
+        "<td>" + parseFloat(tax).toFixed(2) + "</td/>" +
+        "<td>" + parseFloat(Total).toFixed(2) + "</td>" +
+        "<td> <input type='button' value='Remove' name='remove' class='btn btn-danger' onclick='RemoveItem(this)' /> </td></tr>";
 
     tblItemList.append(itemList);
+    FinalItemTotal();
     ResetItem();
 }
 
+function FinalItemTotal() {
+    $("#txtFinalTotal").val("0.00");
+    var finalTotal = 0.00;
+    $("#tblRestaurantItemList").find("tr:gt(0)").each(function () {
+        var tax = parseFloat($(this).find("td:eq(5)").text()); /* equal to index td in tr = tax */
+        var subTotal = parseFloat($(this).find("td:eq(6)").text()); /* equal to index td in tr = total */
+        finalTotal += tax + subTotal;
+    });
+    $("#txtFinalTotal").val(parseFloat(finalTotal).toFixed(2));
+}
 
+/* ToDo: remove from final price */
 function RemoveItem(itemId) {
     $(itemId).closest('tr').remove(); /* Remove closes tr tag */
 }
@@ -51,15 +65,16 @@ function ResetItem() {
     $("#txtPrice").val('');
     $("#txtQuantity").val('');
     $("#txtDiscount").val('0.00');
-    $("#Item").val(0);
+    $("#item").val(0);
     $("#txtTotal").val("0.00");
+    $("#txtTax").val("0.00");
 }
 
 function CalculateSubTotal() { 
     var unitPrice = $("#txtPrice").val();
     var quantity = $("#txtQuantity").val();
     var discount = $("#txtDiscount").val();
-    var tax = (unitPrice * quantity) * .0825;
+    var tax = ((unitPrice * quantity) - discount) * .0825; 
 
     var Total = (unitPrice * quantity) - discount + tax;
     if (Total <= 0) {
@@ -70,11 +85,12 @@ function CalculateSubTotal() {
     }
 }
 
-function CalculateTax() {
+function CalculateSalesTax() {
     var unitPrice = $("#txtPrice").val();
     var quantity = $("#txtQuantity").val();
-    var tax = (unitPrice * quantity) * .0825;
-    $("#txtTax").val(tax.toPrecision(2));
+    var discount = $("#txtDiscount").val();
+    var tax = ((unitPrice * quantity) - discount) * .0825; 
+    $("#txtTax").val(tax.toFixed(2));
 }
 function GetItemPrice(itemId) {
     $.ajax({
